291c92043f5fdf20dec0578f72176a489f253c2a
jrient <jrient@qq.com>
2023-01-10 23:58:10 +0800

    18.02

---
+++ doc/18_短网址项目/src/v1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const AddForm = `
+<html>
+<body>
+<form method="POST" action="/add">
+URL: <input type="text" name="url">
+<input type="submit" value="Add">
+</form>
+</body>
+</html>
+`
+
+var store = NewURLStore()
+
+func main() {
+	http.HandleFunc("/", Redirect)
+	http.HandleFunc("/add", Add)
+	http.ListenAndServe(":9090", nil)
+}
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[1:]
+	url := store.Get(key)
+	if url == "" {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		fmt.Fprint(w, AddForm)
+		return
+	}
+	key := store.Put(url)
+	fmt.Fprintf(w, "http://58.220.230.99:9090/%s", key)
+}
